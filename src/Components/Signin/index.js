07e5a7650281d2d3b79c354bcc54153e21a17487
oliver <olivier.codes@gmail.com>
2021-06-03 20:27:46 +0200

    login validation done

--- src/Components/Signin/index.js
+++ src/Components/Signin/index.js
@@ -1,70 +1,86 @@
-import React from 'react';
-// import {  TextField} from '@material-ui/core'
-// import { CircularProgress } from '@material-ui/core';
-// import { Redirect } from 'react-router-dom';
-import { Slide, Zoom } from 'react-awesome-reveal';
-import { useFormik } from 'formik';
-import * as Yup from 'yup'
+import React, { useState } from 'react';
 
-const Signin = () => {
+import { CircularProgress } from '@material-ui/core';
+// import { Redirect } from 'react-router-dom';
+import { Zoom, Slide } from 'react-awesome-reveal';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const SignIn = () => {
+  const [loading, setLoading] = useState(false)
+
   const formik = useFormik({
     initialValues: {
       email: '',
       password: ''
     },
-    validationsSchema: Yup.object({
-      email: Yup
-          .string('Enter your email')
-          .email('Enter a valide email')
-          .required('The email is required'),
-      password: Yup
-          .string('Enter your password')
-          .min(8, 'Password should be of minimum 8 characters')
-          .required('The email is required'),
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Invalid email address !')
+        .required('The email is required'),
+      password: Yup.string()
+        .min(8, 'The password must have 8 caracters !')
+        .required('The email is required')
     }),
-    onSubmit: (values) =>{
-
+    onSubmit: (values) => {
+      setLoading(true)
+      console.log(values)
     }
-  });
+  })
 
-  const handleSumbit = (e) => {
-    e.preventDefault();
-  }
 
   return (
     <div className="container">
-      <div className="signin_wrapper" style={{margin: '300px'}}>
-      <Zoom top>
-        <h2>Please login</h2>
-      </Zoom>
+      <div className="signin_wrapper" style={{ margin: '100px' }}>
 
-      <Slide left > 
-        <form onSubmit={handleSumbit}>
-        <input
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          placeholder="Enter your email"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        <input
-          name="password"
-          label="Password"
-          type="password"
-          value={formik.values.password}
-          placeholder="Enter your password"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-        />
-        <button type="submit">Log in</button>
-        </form>
+        <Zoom top>
+          <h2>Please login</h2>
+        </Zoom>
+        
+        <Slide left>
+          <form onSubmit={formik.handleSubmit}>
 
-      </Slide>
+            <input
+              name="email"
+              placeholder="Email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+            />
+            {formik.touched.email && formik.errors.email ?
+              <div className="error_label">
+                {formik.errors.email}
+              </div>
+              : null}
+
+
+            <input
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            />
+            {formik.touched.password && formik.errors.password ?
+              <div className="error_label">
+                {formik.errors.password}
+              </div>
+              : null}
+
+
+            {loading ?
+              <CircularProgress color="secondary" className="progress" />
+              :
+              <button type="submit">Log in</button>
+            }
+
+          </form>
+        </Slide>
 
       </div>
     </div>
-  );
-};
+  )
 
-export default Signin;
+}
+
+export default SignIn
